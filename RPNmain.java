b0a8a375c859fd46fd223dbae9b7b59dd7cb5ca9
Felipe Lin <felipejtlin@gmail.com>
2022-04-27 21:15:04 -0300

    RPNStacker

--- RPNmain.java
+++ RPNmain.java
@@ -1,68 +1,119 @@
-import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.Stack;
 
-public class RPNmain {
+import rpntoken.Token;
+import rpntoken.TokenType;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class RPNmain{
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<Integer>();
+        List<Token> tokens = new ArrayList<Token>();
 
         try {
             FileInputStream fstream = new FileInputStream("./test.txt");
             Scanner file = new Scanner(fstream);
 
-
-            while(file.hasNextLine()) {
+            while (file.hasNextLine()) {
                 String line = file.nextLine();
 
-                if(line.equals("")) {
+                if (line.equals("")) {
                     continue;
                 }
 
-                if(!isOperator(line)){
-                    stack.push(Integer.parseInt(line));
-                }
-
-                else{
-                    int num1 = stack.pop();
-                    int num2 = stack.pop();
-
-                    stack.push(calculator(num1, num2, line));
-                } 
-                
+                Token token = tokenType(line);
+                tokens.add(token);
+                System.out.println(token);
             }
+
+            while(tokens.size() != 1) {
+                Token sign = tokens.remove(0);
+                
+                if (sign.type == TokenType.NUM) {
+                    Token sign2 = tokens.remove(0);
+                    if (sign2.type == TokenType.NUM) {
+                        Token sign3 = tokens.remove(0);
+                        if (sign3.type != TokenType.NUM) {
+                            tokens.add(0, calculator(sign, sign2, sign3));     
+                        }
+                        else {
+                            System.out.println("Cannot calculate with 3 numbers in a row");
+                        }
+                    }
+                    else {
+                        System.out.println("Cannot calculate with just 1 number");
+                    }       
+                }
+                else {
+                    System.out.println("Cannot calculate with just a sign");
+                }
+            }
+            
+            System.out.println("Result: " + tokens.get(0).lexeme);
             file.close();
-            System.out.println(stack.pop());
-        }   
-        catch (FileNotFoundException e) {
-            System.out.println("File not found.");
-            e.printStackTrace();
-        } 
+            
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+            ex.printStackTrace();
+
+        } catch (Exception ex){
+            System.out.println("Error");
+            ex.printStackTrace();
+        }
+    }
+    private static Token calculator(Token sign, Token sign2, Token sign3) {
+        String result = "";	
+
+        switch (sign3.lexeme) {
+            case "+":
+                result = Integer.toString(Integer.parseInt(sign.lexeme) + Integer.parseInt(sign2.lexeme));
+                break;
+            case "-":
+                result = Integer.toString(Integer.parseInt(sign.lexeme) - Integer.parseInt(sign2.lexeme));
+                break;
+            case "*":
+                result = Integer.toString(Integer.parseInt(sign.lexeme) * Integer.parseInt(sign2.lexeme));
+                break;
+            case "/":
+                result = Integer.toString(Integer.parseInt(sign.lexeme) / Integer.parseInt(sign2.lexeme));
+                break;
+            default:
+                System.out.println("Cannot calculate with invalid sign");
+                break;
+        }
+
+        return new Token(TokenType.NUM, result);
     }
 
-    public static boolean isOperator(String c) {
-        return (c.equals("+") || c.equals("-")  || c.equals("*")  || c.equals("/") );
+    public static boolean isNumber(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
-    private static int calculator(int num1, int num2, String operator) {
-        int result = 0;
-
-        if(operator.equals("+")){
-            result = num2 + num1;
+    public static Token tokenType(String line) throws Exception{
+        if (line.equals("+")) {
+            return new Token(TokenType.PLUS, line);
         }
-
-        else if(operator.equals("-")){
-            result = num2 - num1;
+        else if (line.equals("-")) {
+            return new Token(TokenType.MINUS, line);
         }
-
-        else if(operator.equals("*")){
-            result = num2 * num1;
+        else if (line.equals("*")) {
+            return new Token(TokenType.STAR, line);
         }
-
-        else if(operator.equals("/")){
-            result = num2 / num1;
+        else if (line.equals("/")) {
+            return new Token(TokenType.SLASH, line);
         }
-
-        return result;
+        else if (isNumber(line)) {
+            return new Token(TokenType.NUM, line);
+        }
+        else{
+            throw new Exception("Error: Unexpected character: " + line);
+        }
     }
-
 }
